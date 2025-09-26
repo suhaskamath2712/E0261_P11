56b8d00e4c162223e18bb51c63356e992daf2583
Suhas Kamath <suhaskamath@iisc.ac.in>
2025-09-26 22:41:48 +0530

    Added some Java code and Calcite dependency

--- plan_equivalence/src/main/java/com/ac/iisc/LLM.java
+++ plan_equivalence/src/main/java/com/ac/iisc/LLM.java
@@ -2,11 +2,10 @@ package com.ac.iisc;
 
 import java.io.IOException;
 
-import okhttp3.OkHttpClient;
-
 import org.json.JSONObject;
 
 import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
